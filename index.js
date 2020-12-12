ae47d2cfb26439863dd92255e7bcb325510d4955
Sergey Shlykov <zeitment@gmail.com>
2020-12-12 20:19:55 +0300

    heroku

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import http from 'http'
-import crypto from 'crypto'
+import express from 'express';
+import bodyParser from 'body-parser';
+import http from 'http';
+import crypto from 'crypto';
 import runner from "./server/app.js";
 import {createReadStream} from 'fs'
 import mongoose from 'mongoose';
-import cors from './utils/cors'
+import cors from './utils/cors';
 import user_model from './models/user';
 
 try {
     const port = process.env.PORT || 3000;
     const user = user_model(mongoose);
-    const app = runner(express, bodyParser, createReadStream, crypto, http, mongoose, user, cors)
-    app.listen(port)
-    console.log(`server is listening at http://localhost:${port}`)
+    const app = runner(express, bodyParser, createReadStream, crypto, http, mongoose, user, cors);
+    app.listen(port);
+    console.log(`server is listening at http://localhost:${port}`);
 } catch (error) {
-    console.log(error)
+    console.log(error);
 }
--- utils/cors.js
+++ utils/cors.js
@@ -1,4 +1,4 @@
-export default CORS = {
+export default {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
     'Access-Control-Allow-Headers': 'x-test,Content-Type,Accept, Access-Control-Allow-Headers',
